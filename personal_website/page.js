c62d7d7f727ce50a78526829cba81d7ac6123c28
prajwal <prajwalkh1997@gmail.com>
2018-06-01 13:23:46 +0530

    portfolio
    its a dummy website i created to try out my skills

---
+++ personal_website/page.js
@@ -0,0 +1,50 @@
+$('.row.segment')
+  .transition({
+    animation : 'jiggle',
+    duration  : 800,
+    interval  : 200
+  })
+;
+// $(function(){
+// 	$("#blog_image").click(function(){
+// 		$(".blog_image").modal({
+// 		closable: true,
+// 		blurring: true,
+
+// 		});
+// 		$(".blog_image").modal('show');	
+// 	});
+// });
+$('.blog_image1')
+	.modal({
+    blurring: true
+  })
+  	.modal('attach events', '#blog_image', 'show')
+  	.modal('attach events', '.blog_image2 .button.prev', 'show')
+;
+
+$('.blogs.modal')
+  .modal({
+    allowMultiple: false
+  })
+;
+$('.blog_image2')
+  .modal('attach events', '.blog_image1 .button')
+;
+
+$('.camp_image1')
+	.modal({
+    blurring: true
+  })
+  	.modal('attach events', '#camp_image', 'show')
+  	.modal('attach events', '.camp_image2 .button.prev', 'show')
+;
+
+$('.camps.modal')
+  .modal({
+    allowMultiple: false
+  })
+;
+$('.camp_image2')
+  .modal('attach events', '.camp_image1 .button')
+;
